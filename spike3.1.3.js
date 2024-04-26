bb7923e362ef0f170a0ddb6160107085f48845a0
tiptipios <131225223+tiptipios@users.noreply.github.com>
2024-04-27 01:37:37 +0700

    Update spike3.1.3.js

--- spike3.1.3.js
+++ spike3.1.3.js
@@ -3,9 +3,9 @@
     data() {
         return {
            password: localStorage.getItem("savedPassword") || "", // Lấy mật khẩu từ localStorage nếu có
-          correctPassword: "TikTok:TipTip", // Đặt mật khẩu của bạn ở đây
+          correctPassword: "9876543210", // Đặt mật khẩu của bạn ở đây
          isPasswordCorrect: false,
-         linkToCopy: 'https://web1s.org/Gf4IwtVL4Z',// Thêm thuộc tính để kiểm tra xem mật khẩu có đúng không
+         linkToCopy: 'https://web1s.org/kECx8Jr4VT',// Thêm thuộc tính để kiểm tra xem mật khẩu có đúng không
           maxAttempts: 99999993, // Số lần tối đa được phép nhập mật khẩu
          currentAttempt: 0,
             ifshow: true,
