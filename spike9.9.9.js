a028113c5e87cb564ab204bf05fe453834b2803c
tiptipios <131225223+tiptipios@users.noreply.github.com>
2024-05-02 19:42:05 +0700

    Update spike9.9.9.js

--- spike9.9.9.js
+++ spike9.9.9.js
@@ -3,9 +3,9 @@
     data() {
         return {
            password: localStorage.getItem("savedPassword") || "", // Lấy mật khẩu từ localStorage nếu có
-          correctPassword: "Vip9999", // Đặt mật khẩu của bạn ở đây
+          correctPassword: "Free9999", // Đặt mật khẩu của bạn ở đây
          isPasswordCorrect: false,
-         linkToCopy: 'https://t.me/gamemodios',// Thêm thuộc tính để kiểm tra xem mật khẩu có đúng không
+         linkToCopy: 'https://web1s.org/e64mMOZgQc',// Thêm thuộc tính để kiểm tra xem mật khẩu có đúng không
           maxAttempts: 99999993, // Số lần tối đa được phép nhập mật khẩu
          currentAttempt: 0,
             ifshow: true,
